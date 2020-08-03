0efc933b8a8cdef6489e2144b3b653a43be67ac8
James Holding <james.holding90@gmail.com>
2020-08-03 19:22:57 -0400

    initial push.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import API from "./utils/API";
+import DataTable from "./components/Table/Data-Table";
 
 function App() {
+  const [employees, setEmployees] = useState([]);
+  useEffect(() => {
+    API.getEmployees().then((res) => {
+      console.log(res);
+      setEmployees(res.data.results);
+    });
+  }, []);
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <Router>
+        <div>
+          <Navbar />
+          <Route
+            exact
+            path="/"
+            render={() => <DataTable employees={employees} />}
+          />
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Table/Data-Table.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { useTable } from "react-table";
+
+function DataTable(props) {
+    const data = React.useMemo(
+        () => props.employees,
+        []
+      )
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Image",
+        accessor: "picture.thumbnail", // accessor is the "key" in the data
+      },
+      {
+        Header: "Name",
+        accessor: "name",
+        Cell: (props) => (
+          <span>
+            {props.first} {props.last}
+          </span>
+        ),
+      },
+      {
+        Header: "Email",
+        accessor: "email", // accessor is the "key" in the data
+      },
+    ],
+    []
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data });
+
+  return (
+    <table {...getTableProps()} style={{ border: "solid 1px blue" }}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th
+                {...column.getHeaderProps()}
+                style={{
+                  borderBottom: "solid 3px red",
+                  background: "aliceblue",
+                  color: "black",
+                  fontWeight: "bold",
+                }}
+              >
+                {column.render("Header")}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return (
+                  <td
+                    {...cell.getCellProps()}
+                    style={{
+                      padding: "10px",
+                      border: "solid 1px gray",
+                      background: "papayawhip",
+                    }}
+                  >
+                    {cell.render("Cell")}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+}
+
+export default DataTable;
