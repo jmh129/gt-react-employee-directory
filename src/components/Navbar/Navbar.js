8cb6b54115c67bddafb1e14bb498d3b80dee0eea
James Holding <james.holding90@gmail.com>
2020-08-03 23:25:56 -0400

    working for most part. added in reference.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,26 @@
 import React from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
+import {
+  Navbar,
+  NavbarBrand,
+  FormGroup,
+  Label,
+  Input,
+} from "reactstrap";
 
 const Navigation = (props) => {
   return (
     <div>
       <Navbar color="light" light expand="md">
         <NavbarBrand href="/">Employee Directory</NavbarBrand>
+        <FormGroup>
+          <Label for="exampleEmail"></Label>
+          <Input
+            type="text"
+            name="input"
+            id="Search"
+            placeholder="Search Last Name"
+          />
+        </FormGroup>
       </Navbar>
     </div>
   );
